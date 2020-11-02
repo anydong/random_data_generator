71bf768b41111ba2ab4d40c1be9dd0ffb907567f
liuzhaowei <liuzhaowei55@gmail.com>
2020-11-02 22:50:48 +0800

    feat: 移除多线程

--- runner/mysql.py
+++ runner/mysql.py
@@ -1,27 +1,58 @@
-import threading
+import datetime
+import time
+from sqlalchemy import Column, BigInteger, String, create_engine, TIMESTAMP
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.sql import func, text
 from generator import address
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from runner import schema
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(BigInteger(), primary_key=True)
+    name = Column(String(255), nullable=False)
+    created_at = Column(TIMESTAMP, nullable=False, server_default=func.now(), default=datetime.datetime.now)
+    updated_at = Column(TIMESTAMP, nullable=False, server_default=text('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+                        default=datetime.datetime.now, onupdate=True)
+    deleted_at = Column(TIMESTAMP, nullable=False, server_default='0000-00-00 00:00:00')
+
+
+# 创建表
+# Base.metadata.create_all(engine)
+
 
 # 初始化数据库连接:
-engine = create_engine('mysql+pymysql://root:123456@127.0.0.1:3306/test')
+engine = create_engine('mysql+pymysql://homestead:secret@127.0.0.1:33060/homestead', pool_size=20, max_overflow=10)
 # 创建DBSession类型:
-DBSession = sessionmaker(bind=engine)
 
 
-def insert():
-    session = DBSession()
-    user = schema.User(
-        name=address.street_name()
-    )
-    session.add(user)
+session_factory = sessionmaker(bind=engine)
+Session = scoped_session(session_factory)
+
+
+def run():
+    start_time = time.time()
+    session = Session()
+
+    for i in range(100):
+        users = []
+        for num in range(1000):
+            users.append(User(
+                name=address.street_name()
+            ))
+        session.add_all(users)
+
     session.commit()
-    session.close()
+    Session.remove()
+    print(time.time() - start_time)
 
 
-for num in range(1000):
-    t = threading.Thread(target=insert, name='ccc')
-    t.start()
-    t.join()
-    print(num)
+if __name__ == "__main__":
+    start_time = time.time()
+    for i in range(100):
+        print(i)
+        run()
+    print('time:', time.time() - start_time)
