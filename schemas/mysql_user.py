ec7a385b98cb5931537314d39d5f0710d4f85453
liuzhaowei <liuzhaowei55@gmail.com>
2020-11-08 23:15:34 +0800

    feat: 生成较为完成的用户数据

--- schemas/mysql_user.py
+++ schemas/mysql_user.py
@@ -1,4 +1,5 @@
 import datetime
+import bcrypt
 from faker import Faker
 from sqlalchemy import Column, BigInteger, String, TIMESTAMP
 from sqlalchemy.ext.declarative import declarative_base
@@ -8,18 +9,21 @@ Base = declarative_base()
 
 
 def url():
-    return 'mysql+pymysql://homestead:secret@127.0.0.1:33060/homestead?charset=utf8mb4'
+    return 'mysql+pymysql://root:123456@127.0.0.1:3306/test?charset=utf8mb4'
 
 
 class User(Base):
     __tablename__ = 'user'
 
     id = Column(BigInteger(), primary_key=True)
-    name = Column(String(255), nullable=False)
-    created_at = Column(TIMESTAMP, nullable=False, server_default=func.now(), default=datetime.datetime.now)
+    username = Column(String(255), nullable=False)
+    password = Column(String(255), nullable=False)
+    nickname = Column(String(255), nullable=False)
+    avatar = Column(String(255), nullable=False)
+    email = Column(String(255), nullable=False)
     updated_at = Column(TIMESTAMP, nullable=False, server_default=text('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
                         default=datetime.datetime.now, onupdate=True)
-    deleted_at = Column(TIMESTAMP, nullable=False, server_default='0000-00-00 00:00:00')
+    created_at = Column(TIMESTAMP, nullable=False, server_default=func.now(), default=datetime.datetime.now)
 
 
 fake = Faker(locale='zh_CN')
@@ -27,5 +31,9 @@ fake = Faker(locale='zh_CN')
 
 def generator():
     return User(
-        name=fake.name()
+        username=fake.name(),
+        password=fake.lexify(text='????????????????'),
+        nickname=fake.lexify(text='????????????????'),
+        avatar=fake.image_url(),
+        email=fake.email()
     )
