6006d80cc73d9eef1421a9ae79bef168d4489124
where.liu <where.liu@huolala.cn>
2020-11-10 09:35:22 +0800

    feat: switch

--- main.py
+++ main.py
@@ -7,9 +7,24 @@ def main():
     parser.add_argument('-d', '--driver', choices=['mysql', 'mongodb'], default='mysql', help="连接数据库类型，默认：mysql")
     parser.add_argument('-n', '--number', dest='number', type=int, default=1, help="生成数据量（万），默认：1 万")
     args = parser.parse_args()
-    print('driver:', args.driver)
-    print('schema:', args.schema)
-    print('number:', args.number)
+
+    drivers = {
+        'mysql': mysql,
+        'mongodb': mongodb
+    }
+    drivers.get(args.driver)(args.schema, args.number)
+
+
+def mysql(schema: str, number: int):
+    print("mysql")
+    print('schema:', schema)
+    print('number:', number)
+
+
+def mongodb(schema: str, number: int):
+    print("mongodb")
+    print('schema:', schema)
+    print('number:', number)
 
 
 if __name__ == '__main__':
